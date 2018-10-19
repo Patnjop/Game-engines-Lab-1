55a1d81d8f582872b96ceac0e1c06134ccb068a4
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-10-19 21:14:11 +0100

    Tower Rotating

--- New folder/Assets/TowerCreator.cs
+++ New folder/Assets/TowerCreator.cs
@@ -7,26 +7,45 @@ public class TowerCreator : MonoBehaviour
 
     float addedAngle;
     float cubeCount;
-
+    bool even;
     public int circleRadius;
     public int heightCount;
     float cubeX;
     float cubeZ;
     float cubeCirc;
     float cubeHeight = 0.5f;
-    int increments;
+    float increments;
+    public Collider coll;
 
+    RaycastHit hit;
     // Use this for initialization
 
     void Start()
     {
 
         cubeCirc = Mathf.PI * 2f * circleRadius;
-
-        cubeCount = cubeCirc / 1.3f;
+        coll = GetComponent<Collider>();
+        cubeCount = cubeCirc / 1.5f;
         addedAngle = 2f * Mathf.PI / cubeCount;
+    }
 
-        //BuildTower();
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool didhit = Physics.Raycast(ray, out hit, 500.0f);
+            if (didhit)
+            {
+                BuildTower();
+            }  
+
+        }
+    }
+
+    void BuildTower()
+    {
         for (int j = 0; j < heightCount; j++)
         {
             for (int i = 0; i < cubeCount; i++)
@@ -35,17 +54,22 @@ public class TowerCreator : MonoBehaviour
                 cubeX = Mathf.Sin(addedAngle * increments) * circleRadius;
                 cubeZ = Mathf.Cos(addedAngle * increments) * circleRadius;
                 cube.transform.position = new Vector3(cubeX, cubeHeight, cubeZ);
-                cube.transform.rotation.SetAxisAngle(cube.transform.position, addedAngle);
+                cube.transform.Rotate(0, (addedAngle * increments) * Mathf.Rad2Deg, 0);
+                cube.GetComponent<Renderer>().material.color = Random.ColorHSV(0.4f, 0.8f, 0.8f, 1f, 0f, 1f);
                 cube.AddComponent<Rigidbody>();
                 increments++;
+                even = !even;
+            }
+            if (even)
+            {
+                increments = 0.5f;
+            }
+            else
+            {
+                increments = 0;
             }
             cubeHeight += 1f;
         }
-    }
-
-    void BuildTower()
-    {
-        Debug.Log(cubeCount);
     }
 }
 
